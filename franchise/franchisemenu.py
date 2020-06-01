d84ff534f3c150e0469db9060694f3ee733c1690
Levy <amlevy@wpi.edu>
2020-05-31 22:40:57 -0400

    team options schedule ui

--- franchise/franchisemenu.py
+++ franchise/franchisemenu.py
@@ -7,7 +7,6 @@ import logic
 
 def determineTeams(f):
     #determines the home and away teams of the next user game
-    day = f["info"][0]["day"]
     daygames = f["schedule"][int(day)]
     for i in daygames:
         for j in i:
@@ -15,7 +14,7 @@ def determineTeams(f):
                 return i
 
 def loopImages(f):
-    global menuplay, menuteam, menuleague, leagueOptionsButtons
+    global menuplay, menuteam, menuleague, leagueOptionsButtons, teamOptionsButtons, left_observe, right_observe
 
     #background
     glovars.screen.blit(pygame.image.load("assets/images/franchiseBG.png"), (0,0))
@@ -86,25 +85,92 @@ def loopImages(f):
         pygame.draw.rect(glovars.screen,glovars.black,(332,229,62,42),0)
         glovars.message_display("vs.", 341, 229, glovars.teamSelectFont, glovars.white)
         
+    #default all teamOptions and team buttons to false
+    teamOptionsButtons = [False] * 6
+
+    #default no schedule being observed
+    left_observe = False
+    right_observe = False
+    
     # TEAM MENU
     if selected_menu == 1:
         pygame.draw.rect(glovars.screen,glovars.white,(394,76,234,58),0) #selected
 
-         #fill in outline borders for team options
-        pygame.draw.rect(glovars.screen,glovars.white,(94,152,534,400),0)
-        pygame.draw.rect(glovars.screen,glovars.black,(98,354,261,194),0)
-        pygame.draw.rect(glovars.screen,glovars.black,(363,354,261,194),0)
-        pygame.draw.rect(glovars.screen,glovars.black,(98,156,261,194),0)
-        pygame.draw.rect(glovars.screen,glovars.black,(363,156,261,194),0)
+        if teamOptions == "menu":
+            #fill in outline borders for team options
+            pygame.draw.rect(glovars.screen,glovars.white,(94,152,534,400),0)
+            teamOptionsButtons[2] = pygame.draw.rect(glovars.screen,glovars.black,(98,354,261,194),0)
+            teamOptionsButtons[3] = pygame.draw.rect(glovars.screen,glovars.black,(363,354,261,194),0)
+            teamOptionsButtons[0] = pygame.draw.rect(glovars.screen,glovars.black,(98,156,261,194),0)
+            teamOptionsButtons[1] = pygame.draw.rect(glovars.screen,glovars.black,(363,156,261,194),0)
 
-        #team options
-        glovars.message_display("roster",110,308,glovars.teamFont30,glovars.white)
-        glovars.message_display("schedule",375,308,glovars.teamFont30,glovars.white)
-        glovars.message_display("trade",110,506,glovars.teamFont30,glovars.white)
-        glovars.message_display("free agents",375,506,glovars.teamFont30,glovars.white)
+            #team options
+            glovars.message_display("roster",110,308,glovars.teamFont30,glovars.white)
+            glovars.message_display("schedule",375,308,glovars.teamFont30,glovars.white)
+            glovars.message_display("trade",110,506,glovars.teamFont30,glovars.white)
+            glovars.message_display("free agents",375,506,glovars.teamFont30,glovars.white)
+        elif teamOptions == "roster":
+            pygame.draw.rect(glovars.screen,glovars.white,(94,152,534,400),0)
+
+        # SCHEDULE
+        elif teamOptions == "schedule":
+
+            #set the matchup observed
+            matchup = day_observe
+
+            pygame.draw.rect(glovars.screen,glovars.white,(94,152,534,400),0)
+            pygame.draw.rect(glovars.screen,glovars.black,(98,156,526,392),0)
+            pygame.draw.rect(glovars.screen,glovars.white,(294,548,334,50),0)
+            pygame.draw.rect(glovars.screen,glovars.black,(298,552,326,42),0)
+            pygame.draw.rect(glovars.screen,glovars.white,(516,548,112,50),0)
+            left_observe = pygame.draw.rect(glovars.screen,glovars.black,(520,552,50,42),0)
+            right_observe = pygame.draw.rect(glovars.screen,glovars.black,(574,552,50,42),0)
+            if matchup == 0:
+                weektype = "Preseason"
+            else:
+                weektype = "matchup " + str(matchup)
+            glovars.message_display(weektype,306,555,glovars.teamFont30,glovars.white)
+
+            #display the schedule for the week
+            for i in range(3):
+
+                #offsets for match on right side
+                xdist = 117
+                goalfilloffset = 0
+                goalsoffset = 0
+                borderoffset = 0
+                if i == 1:
+                    xdist = 379
+                    borderoffset = -54
+                    goalfilloffset = -284
+                    goalsoffset = -22
+
+                #determine home and away teams, add outline, and blit to screen
+                awayschedule = f["schedule"][day_observe][i][0]
+                homeschedule = f["schedule"][day_observe][i][1]
+                pygame.draw.rect(glovars.screen,glovars.white,(xdist - 4 + borderoffset, 171 + (125 * i), 288, 112),0)
+                glovars.screen.blit(glovars.defaultTeams[homeschedule].scorebug, (xdist,229 + (125 * i)))
+                glovars.screen.blit(glovars.defaultTeams[awayschedule].scorebug, (xdist,175 + (125 * i)))
+
+                #determine scores, add fill, and blit to screen
+                pygame.draw.rect(glovars.screen,glovars.black,(xdist + 230 + goalfilloffset, 175 + (125 * i), 50, 50),0)
+                pygame.draw.rect(glovars.screen,glovars.black,(xdist + 230 + goalfilloffset, 229 + (125 * i), 50, 50),0)
+                if day_observe < day:
+                    texthome = glovars.EASmall50.render(str((f["results"][day_observe][i][0]["Goals"])), True, glovars.white)
+                    texthome_rect = texthome.get_rect(center = (372 + goalsoffset, 254 + (125 * i)))
+                    textaway = glovars.EASmall50.render(str((f["results"][day_observe][i][1]["Goals"])), True, glovars.white)
+                    textaway_rect = textaway.get_rect(center = (372 + goalsoffset, 200 + (125 * i)))
+                    glovars.screen.blit(texthome, texthome_rect)
+                    glovars.screen.blit(textaway, textaway_rect)
+
+
+        elif teamOptions == "trade":
+            pygame.draw.rect(glovars.screen,glovars.white,(94,152,534,400),0)
+        elif teamOptions == "free agents":
+            pygame.draw.rect(glovars.screen,glovars.white,(94,152,534,400),0)
 
     #default all leagueOptions and league buttons to false
-    leagueOptionsButtons = [False] * 4
+    leagueOptionsButtons = [False] * 6
 
     # LEAGUE MENU
     if selected_menu == 2:
@@ -126,7 +192,6 @@ def loopImages(f):
 
         # STANDINGS
         elif leagueOptions == "standings":
-            
             pygame.draw.rect(glovars.screen,glovars.white,(94,152,534,400),0)
             pygame.draw.rect(glovars.screen,glovars.black,(98,156,526,392),0)
             glovars.message_display("standings",116,180,glovars.teamFont30,glovars.googusGreen)
@@ -135,6 +200,7 @@ def loopImages(f):
             glovars.message_display("ol",503,180,glovars.teamFont30,glovars.googusGreen)
             glovars.message_display("p",575,180,glovars.teamFont30,glovars.googusGreen)
  
+            #order teams by points
             order = logic.calculateRankings(f)
             for i in range(len(order)):
                 glovars.message_display(order[i]["mascot"],116,240 + (i * 50),glovars.teamFont30,glovars.white)
@@ -144,15 +210,29 @@ def loopImages(f):
                 glovars.message_display(str(f["teamdata"][0][order[i]["name"]][0]["overtimelosses"]),512,240 + (i * 50),glovars.teamFont30,glovars.white)
 
         # STATS
-        elif leagueOptions == "stats":
+        elif leagueOptions == "stats":            
             pygame.draw.rect(glovars.screen,glovars.white,(94,152,534,400),0)
+            leagueOptionsButtons[4] = pygame.draw.rect(glovars.screen,glovars.black,(98,156,526,194),0)
+            leagueOptionsButtons[5] = pygame.draw.rect(glovars.screen,glovars.black,(98,354,526,194),0)
+            glovars.message_display("player stats",110,308,glovars.teamFont30,glovars.white)
+            glovars.message_display("team stats",110,506,glovars.teamFont30,glovars.white)
+        elif leagueOptions == "team stats":
+            pygame.draw.rect(glovars.screen,glovars.white,(94,152,534,400),0)
+            pygame.draw.rect(glovars.screen,glovars.black,(98,156,526,392),0)
+            glovars.message_display("team stats",116,180,glovars.teamFont30,glovars.googusGreen)
+            glovars.message_display("gf",375,180,glovars.teamFont30,glovars.googusGreen)
+            glovars.message_display("ga",442,180,glovars.teamFont30,glovars.googusGreen)
+            glovars.message_display("e",517,180,glovars.teamFont30,glovars.googusGreen)
+            glovars.message_display("e",575,180,glovars.teamFont30,glovars.googusGreen)
+        elif leagueOptions == "player stats":  
+            pygame.draw.rect(glovars.screen,glovars.white,(94,152,534,400),0)
+
         # HISTORY
         elif leagueOptions == "history":
             pygame.draw.rect(glovars.screen,glovars.white,(94,152,534,400),0)
         # PRESTIGE
         elif leagueOptions == "prestige":
             pygame.draw.rect(glovars.screen,glovars.white,(94,152,534,400),0)
-
 
     #fill menu options
     menuplay = pygame.draw.rect(glovars.screen,tintmenus[0],(98,80,226,50),0)
@@ -165,11 +245,63 @@ def loopImages(f):
     glovars.message_display("league",763,88,glovars.EASmall30,glovars.white)
 
 
+def optionsCheck(teamOptions, leagueOptions, oday, f):
+    global day_observe
+
+    day_observe = oday
+
+    #TEAM OPTIONS
+    if teamOptionsButtons[0] != False:
+        if teamOptionsButtons[0].collidepoint(pygame.mouse.get_pos()):
+            teamOptions = "roster"
+        if teamOptionsButtons[1].collidepoint(pygame.mouse.get_pos()):
+            teamOptions = "schedule"
+            day_observe = day
+        else:
+            day_observe = f["info"][0]["day"]
+        if teamOptionsButtons[2].collidepoint(pygame.mouse.get_pos()):
+            teamOptions = "trade"
+        if teamOptionsButtons[3].collidepoint(pygame.mouse.get_pos()):
+            teamOptions = "free agents"
+    if left_observe:
+        if left_observe.collidepoint(pygame.mouse.get_pos()):
+            day_observe -= 1
+            if day_observe < 0:
+                day_observe = len(f["schedule"]) - 1
+        if right_observe.collidepoint(pygame.mouse.get_pos()):
+            day_observe += 1
+            if day_observe > len(f["schedule"]) - 1:
+                day_observe = 0
+
+
+    #LEAGUE OPTIONS
+    if leagueOptionsButtons[0] != False:
+        if leagueOptionsButtons[0].collidepoint(pygame.mouse.get_pos()):
+            leagueOptions = "standings"
+        if leagueOptionsButtons[1].collidepoint(pygame.mouse.get_pos()):
+            leagueOptions = "stats"
+        if leagueOptionsButtons[2].collidepoint(pygame.mouse.get_pos()):
+            leagueOptions = "history"
+        if leagueOptionsButtons[3].collidepoint(pygame.mouse.get_pos()):
+            leagueOptions = "prestige"
+    if leagueOptionsButtons[4] != False:
+        if leagueOptionsButtons[4].collidepoint(pygame.mouse.get_pos()):
+            leagueOptions = "player stats"
+        if leagueOptionsButtons[5].collidepoint(pygame.mouse.get_pos()):
+            leagueOptions = "team stats"
+
+    return teamOptions, leagueOptions, day_observe
+
+
 def runMenu(f, menu):
-    global tintmenus, allmenus, selected_menu, leagueOptions
+    global tintmenus, allmenus, selected_menu, leagueOptions, teamOptions, day
 
     #play, team, league
     selected_menu = menu
+
+    #default options to the menu
+    teamOptions = "menu"
+    leagueOptions = "menu"
 
     exitLoop = False #exit game
 
@@ -178,6 +310,11 @@ def runMenu(f, menu):
 
     appPressed = False #if an app is pressed on phone 
     loops = 315 #number of loops - defaults to maximun when no app is pressed
+
+    #current matchup day
+    day = f["info"][0]["day"]
+    #current schedule observe day
+    oday = day
 
     loopImages(f)
 
@@ -197,19 +334,14 @@ def runMenu(f, menu):
                     selected_menu = 0
                 if menuteam.collidepoint(pygame.mouse.get_pos()):
                     selected_menu = 1
+                    teamOptions = "menu"
                 if menuleague.collidepoint(pygame.mouse.get_pos()):
                     selected_menu = 2
                     leagueOptions = "menu"
-                for i in range(len(leagueOptionsButtons)):
-                    if leagueOptionsButtons[i] != False:
-                        if leagueOptionsButtons[0].collidepoint(pygame.mouse.get_pos()):
-                            leagueOptions = "standings"
-                        if leagueOptionsButtons[1].collidepoint(pygame.mouse.get_pos()):
-                            leagueOptions = "stats"
-                        if leagueOptionsButtons[2].collidepoint(pygame.mouse.get_pos()):
-                            leagueOptions = "history"
-                        if leagueOptionsButtons[3].collidepoint(pygame.mouse.get_pos()):
-                            leagueOptions = "prestige"
+
+                #check if team and league options have been selected
+                teamOptions, leagueOptions, oday = optionsCheck(teamOptions, leagueOptions, oday, f)
+
             if event.type == pygame.KEYUP:
                 if event.key == pygame.K_y:
                     appPressed = not appPressed
